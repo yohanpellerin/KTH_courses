d5c060f0597d46aa5f7bbee02912309bc015cc84
Pellerin <yohan.pellerin@ecl21.ec-lyon.fr>
2024-08-21 15:20:38 +0200

    last_docs

---
+++ P2/AI/projects/rl/rl5/rl5/student_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Q learning learning rate
+alpha = 0.5
+
+# Q learning discount rate
+gamma = 0.8
+
+# Epsilon initial
+epsilon_initial = 0.5
+
+# Epsilon final
+epsilon_final = 0.1
+
+# Annealing timesteps
+annealing_timesteps = 10000
+
+# threshold
+threshold = 1e-6
